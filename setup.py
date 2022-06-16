04128c362252296fabfd2e1e0cd2dfb66e0763a9
erguntiryaki <erguntiryaki27@gmail.com>
2022-06-16 09:43:01 +0300

    recreate setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='scanalysis',
+    version='0.1.0',
+    packages=['scanalysis'],
+    url='',
+    license='',
+    author='ergun',
+    author_email='erguntiryaki27@gmail.com',
+    description='',
+    install_requires=['scanpy']
+)
